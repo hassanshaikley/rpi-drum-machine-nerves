3b0f806945b13b93f62fdc55e2f4e82bdc3fb27e
Hassan Shaikley <hassan.shaikley@gmail.com>
2019-10-31 02:17:53 -0700

    Initial commit

---
+++ config/host.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+config :rpi_music_machine_nerves, :viewport, %{
+  name: :main_viewport,
+  # default_scene: {RpiMusicMachineNerves.Scene.Crosshair, nil},
+  default_scene: {RpiMusicMachineNerves.Scene.Crosshair, nil},
+  size: {800, 480},
+  opts: [scale: 1.0],
+  drivers: [
+    %{
+      module: Scenic.Driver.Glfw,
+      opts: [title: "MIX_TARGET=host, app = :rpi_music_machine_nerves"]
+    }
+  ]
+}
