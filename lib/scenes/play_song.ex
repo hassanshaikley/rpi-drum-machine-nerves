63ba706b7b7ea3302ac5f63941d8ccce6a32b59f
Hassan Shaikley <hassan.shaikley@gmail.com>
2019-11-03 04:21:03 -0800

    WIP

--- lib/scenes/play_song.ex
+++ lib/scenes/play_song.ex
@@ -3,6 +3,7 @@ defmodule RpiMusicMachineNerves.Scene.PlaySong do
   alias Scenic.Graph
 
   import Scenic.Primitives
+  import Scenic.Primitive.Text
 
   @fps 10
   @screen_width 800
@@ -13,13 +14,14 @@ defmodule RpiMusicMachineNerves.Scene.PlaySong do
     [text: "I have found in this place", start_time: 4000, time_end: ~T[01:00:07.001], note: :c]
   ]
   @graph Graph.build(font_size: 22, font: :roboto_mono)
-         |> group(
-           fn g ->
-             g
-             |> text("Hello friend Playing song", translate: {10, 20}, font_size: 18)
-           end,
-           t: {10, 30}
-         )
+         #  |> group(
+         #    fn g ->
+         #      g
+         #      |> text("Hello friend Playing song", translate: {10, 20}, font_size: 18)
+         #    end,
+         #    t: {10, 30}
+         #  )
+         #  |> text("Hi", id: :hi, translate: {50, 80})
          |> text("CURRENT LYRIC FIELD", id: :current_lyric, translate: {150, 80})
          |> Map.put(:current_lyric_index, 0)
 
@@ -43,20 +45,18 @@ defmodule RpiMusicMachineNerves.Scene.PlaySong do
   end
 
   def handle_info(:show_next_lyric, graph) do
+    current_lyric = Enum.at(@in_the_aeroplane, graph.current_lyric_index)
+
+    current_lyric_text = Keyword.get(current_lyric, :text)
     graph = Map.put(graph, :current_lyric_index, graph.current_lyric_index + 1)
-    IO.puts("Showing next lyric")
 
     graph =
-      Graph.modify(graph, :current_lyric, fn p ->
-        Primitive.put(
-          p,
-          "poop"
-        )
-      end)
+      graph
+      |> Graph.modify(:current_lyric, &text(&1, current_lyric_text))
 
     IO.puts("Putting poop")
 
-    {:noreply, graph}
+    {:noreply, graph, push: graph}
   end
 
   @impl true
