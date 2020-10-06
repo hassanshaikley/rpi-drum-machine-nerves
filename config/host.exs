bab068a08c4d64ce4f6eecde5012c3b0d1c6f132
Hassan Shaikley <hassan.shaikley@gmail.com>
2020-10-06 13:14:13 -0700

    Initial commit

---
+++ config/host.exs
@@ -0,0 +1,15 @@
+use Mix.Config
+
+config :drum_machine_nerves, :viewport, %{
+  name: :main_viewport,
+  # default_scene: {DrumMachineNerves.Scene.Crosshair, nil},
+  default_scene: {DrumMachineNerves.Scene.SysInfo, nil},
+  size: {800, 480},
+  opts: [scale: 1.0],
+  drivers: [
+    %{
+      module: Scenic.Driver.Glfw,
+      opts: [title: "MIX_TARGET=host, app = :drum_machine_nerves"]
+    }
+  ]
+}
