f48b19cb786ce47313f807efae38278f9f98dc9d
Hassan Shaikley <hassan.shaikley@gmail.com>
2021-07-30 11:47:06 -0400

    wip macro vol

--- .iex.exs
+++ .iex.exs
@@ -1,10 +1,35 @@
-thing = fn 0 -> 1 end
+alias RpiDrumMachineNerves.Components.VolumeControls
+
+volume_text_macro = fn x ->
+
+  VolumeControls.volume_text_macro(x)
+end
+
+
+volume_text = fn x ->
+
+  VolumeControls.volume_text(x)
+end
+
+# Benchee.run(
+#   %{
+#     "rem" => fn -> rem(1 + 1, 8) end,
+#     "match" => fn -> RpiDrumMachineNerves.Optimizations.get_next_iteration(0) end
+#   },
+#   time: 10,
+#   memory_time: 2
+# )
+
+
+
 
 Benchee.run(
   %{
-    "rem" => fn -> rem(1 + 1, 8) end,
-    "match" => fn -> RpiDrumMachineNerves.Optimizations.get_next_iteration(0) end
+    "without macro" => fn -> volume_text.(10) end,
+    "with macro" => fn -> volume_text_macro.(10) end
   },
   time: 10,
   memory_time: 2
 )
+
+# IO.puts "HI"
--- lib/components/volume_controls.ex
+++ lib/components/volume_controls.ex
@@ -52,9 +52,13 @@ defmodule RpiDrumMachineNerves.Components.VolumeControls do
   def verify(_), do: {:ok, nil}
 
   def handle_cast({:update_volume, new_volume}, state) do
-    vol = Integer.to_string(new_volume)
-    graph = Graph.modify(state.graph, :volume_label, &text(&1, "vol\n(" <> vol <> ")"))
+    graph = Graph.modify(state.graph, :volume_label, &text(&1, volume_text_macro(new_volume)))
     {:noreply, state, push: graph}
+  end
+
+  def volume_text(vol) do
+    vol = Integer.to_string(vol)
+    "vol\n(" <> vol <> ")"
   end
 
   def child_spec({args, opts}) do
@@ -66,5 +70,13 @@ defmodule RpiDrumMachineNerves.Components.VolumeControls do
       restart: :permanent,
       shutdown: 500
     }
+  end
+
+
+  0..100 |> Enum.each fn vol ->
+    def volume_text_macro(unquote(vol)) do
+      vol_string = to_string(unquote(vol))
+      "vol\n(" <> vol_string <> ")"
+    end
   end
 end
